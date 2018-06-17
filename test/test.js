4175fb4bc8a2e4f4b19852cb105b0020bf7cecde
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2018-06-16 21:11:40 -0400

    Adding relationship methods
    One to One, On to Many and Many to Many, including also one to one inverse.

--- test/test.js
+++ test/test.js
@@ -9,22 +9,63 @@ const pool = new Pool({
   port: 5432
 });
 
-class profiles extends Nightmare {
-  constructor(){
-    super(pool, 'profiles');
-    this.attributes = ['name']
+class Profiles extends Nightmare {
+  constructor(data = false){
+    super(pool, 'profiles', data);
+    this.user_id = 1;
   }
 
-  async all(){
-    let result  = await this.select('*').limit(2).execute();
-    console.log(result);
+  async user(instanceOf = false){
+    return await this.belongsTo(instanceOf, 'user_id', 'id', 'users');
+  }
+}
+class User extends Nightmare {
+  constructor(data = false){
+    super(pool, 'users', data);
+    this.id = 1;
+  }
+  async profile(instanceOf = false){
+    return await this.hasOne(instanceOf, 'id', 'user_id', 'profiles');
   }
 }
 
-let pro = new profiles();
-pro.count("*")
-.where({
-  type: 'TALENT'
-})
-.execute()
-.then(result => console.log(result));
+class Video extends Nightmare {
+  constructor(data = false){
+    super(pool, 'videos', data);
+  }
+
+  async comments(instanceOf = Comment, sort = 'ASC'){
+    return await this.hasMany(instanceOf, 'id', 'video_id', 'comments', sort);
+  }
+  hashtags(instanceOf = Hastag, sort = 'ASC'){
+    return this.belongsToMany(instanceOf, 'hashtag__videos', 'hashtags', 'video_id', 'hashtag_id', sort);
+  }
+}
+
+class Comment extends Nightmare {
+  constructor(data = false){
+    super(pool, 'comments', data);
+  }
+  async video(instanceOf = false){
+    return await this.belongsTo(instanceOf, 'video_id', 'id', 'videos');
+  }
+}
+
+class Hastag extends Nightmare {
+  constructor(data = false){
+    super(pool, 'hashtags', data);
+  }
+  videos(instanceOf = Video, sort = 'ASC'){
+    return this.belongsToMany(instanceOf, 'hashtag__videos', 'videos', 'hashtag_id', 'video_id', sort);
+  }
+}
+let vid = new Video();
+let com = new Comment();
+let hash = new Hastag();
+// let pro = new Profiles(), user = new User();
+// user.profile().then(profile => console.log(profile));
+// pro.user().then(user => console.log(user));
+// vid.id = 1;
+// vid.comments().then(comments => console.log(comments));
+hash.id = 322;
+hash.videos(false, 'desc').then(videos => console.log(videos));
